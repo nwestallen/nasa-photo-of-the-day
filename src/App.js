1fa32d50cd4e9132c5effe0fa08fa715bcf26809
Nick Allen <nwestallen@gmail.com>
2021-01-20 19:39:02 -0500

    successfully retrieved photo from NASA API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Photo from "./components/Photo"
+import Photo from "./components/Photo";
+import { BASE_URL, API_KEY } from './constants/index';
+import axios from 'axios';
 
 function App() {
   const [url, setUrl] = useState()
 
   useEffect(() => {
     console.log('Side effect');
-    setUrl('https://static2.srcdn.com/wordpress/wp-content/uploads/2019/02/Danny-DeVito-on-Its-Always-Sunny-in-Philadelphia.jpg')
+    //setUrl('https://static2.srcdn.com/wordpress/wp-content/uploads/2019/02/Danny-DeVito-on-Its-Always-Sunny-in-Philadelphia.jpg');
+    axios
+    .get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      setUrl(res.data.url);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }, []);
 
   return (
