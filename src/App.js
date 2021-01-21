86fe706435ad1b747a9d0efd876421ed5fc10771
Nick Allen <nwestallen@gmail.com>
2021-01-20 21:29:45 -0500

    changed data response & added load text

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import Photo from "./components/Photo";
 import { BASE_URL, API_KEY } from './constants/index';
 import axios from 'axios';
 
+const dummyData = {
+  "date": "2021-01-20",
+  "explanation": "Do magnetic fields always flow along spiral arms?  Our face-on view of the Whirlpool Galaxy (M51) allows a spectacularly clear view of the spiral wave pattern in a disk-shaped galaxy.  When observed with a radio telescope, the magnetic field appears to trace the arms' curvature.  However, with NASA’s flying Stratospheric Observatory for Infrared Astronomy (SOFIA) observatory, the magnetic field at the outer edge of M51's disk appears to weave across the arms instead.  Magnetic fields are inferred by grains of dust aligning in one direction and acting like polaroid glasses on infrared light.  In the featured image, the field orientations determined from this polarized light are algorithmically connected, creating streamlines.  Possibly the gravitational tug of the companion galaxy, at the top of the frame, on the dusty gas of the reddish star-forming regions, visible in the Hubble Space Telescope image, enhances turbulence -- stirring the dust and lines to produce the unexpected field pattern of the outer arms.",
+  "hdurl": "https://apod.nasa.gov/apod/image/2101/M51Bfield_Sofia_2286.jpg",
+  "media_type": "image",
+  "service_version": "v1",
+  "title": "The Magnetic Field of the Whirlpool Galaxy",
+  "url": "https://apod.nasa.gov/apod/image/2101/M51Bfield_Sofia_960.jpg"
+  }
+
+
 function App() {
-  const [url, setUrl] = useState()
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    console.log('Side effect');
-    //setUrl('https://static2.srcdn.com/wordpress/wp-content/uploads/2019/02/Danny-DeVito-on-Its-Always-Sunny-in-Philadelphia.jpg');
     axios
     .get(`${BASE_URL}?api_key=${API_KEY}`)
     .then(res => {
-      setUrl(res.data.url);
+      console.log(res)
+      setData(res.data);
     })
     .catch(err => {
       console.log(err);
     })
   }, []);
+
 
   return (
     <div className="App">
@@ -26,7 +37,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
-      <Photo url={url}/>
+      <Photo url={data.url}/>
     </div>
   );
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,7 +4,16 @@ export default function Photo(props) {
     const { url } = props;
     const [photoData, setPhotoData] = useState([]);
 
+    if(!props.url) {
+        return(
+            <h2>Loading. . . </h2>
+        )
+    };
+
     return (
-        <img src={props.url} alt='Danny' />
+        <div className='photoFrame'>
+            <img src={props.url} alt='Danny' />
+            <p className='caption'>Danny Boy</p>
+        </div>
     )
 }
